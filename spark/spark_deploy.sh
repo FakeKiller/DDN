306fb5b8df8e6d3c3f3eaee883e85a701edbac33
Shijie Sun <septimus145@gmail.com>
2016-08-31 03:45:45 -0400

    Combine Uploader and Coordinator and rewrite it using spark

--- spark/spark_deploy.sh
+++ spark/spark_deploy.sh
@@ -26,6 +26,7 @@ wget http://www-eu.apache.org/dist/spark/spark-1.6.2/spark-1.6.2-bin-hadoop2.6.t
 sudo tar -xvzf spark-1.6.2-bin-hadoop2.6.tgz -C /usr/share
 sudo mv /usr/share/spark-1.6.2-bin-hadoop2.6 /usr/share/spark
 rm spark-1.6.2-bin-hadoop2.6.tgz
+echo "spark.io.compression.codec    lzf" | sudo tee --append /usr/share/spark/conf/spark-defaults.conf
 
 sudo mkdir -p /var/spark_tmp
 sudo cp ./entry.dat /var/spark_tmp/
